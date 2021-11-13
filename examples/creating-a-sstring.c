6e323223f7b4cea3adc8f7fb77cb6b06d062bdae
Prakash Niroula <82296809+Pr-World@users.noreply.github.com>
2021-11-13 19:58:50 +0545

    example usage help - added

---
+++ examples/creating-a-sstring.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sstring.h"
+
+
+/*
+Example usage of declaring a string!
+initially we dont need to include stdlib.h
+But are included for functions defined here!
+*/
+
+//User can handle errors in their **Own** Way
+
+void checkforerrs(sstring str){
+	if(str._err!=_SSTRING_NO_ERR){
+		sstr.showerrs(str._err);
+		exit(str._err);
+	}
+}
+
+
+int main(){
+	//creating sstring
+	sstring a;
+	a = sstr.newstr("Hello World!");
+	
+	checkforerrs(a);
+	
+	//display
+	printf("%s",sstr.val(a));
+	
+	return 0;
+}
